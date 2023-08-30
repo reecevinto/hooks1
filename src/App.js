2ecf23c45c033995c604c3a1b8649c9c542fb06a
Collins Murgor <vintoncollins702@gmail.com>
2023-08-30 11:05:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  function handleClick() {
+    setIndex(index + 1);
+  }
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name}</i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculpture.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
+  );
+}
